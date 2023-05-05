20439533071b250fa27763901303c802ad4d860c
Yazid <yazeedkurdi@hotmail.com>
2023-05-05 18:29:31 +0300

    rename folder

--- backend/settings.py
+++ backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.static',
     # third party apps
     'rest_framework',
     'rest_framework.authtoken',
@@ -175,7 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'))
