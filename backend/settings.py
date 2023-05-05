64337441c69cd4da235f9a5e83aee480d6ce6773
Yazid <yazeedkurdi@hotmail.com>
2023-05-05 19:41:55 +0300

    statitc

--- backend/settings.py
+++ backend/settings.py
@@ -175,7 +175,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # # Extra places for collectstatic to find static files.
 # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'frontend/build/static'))
