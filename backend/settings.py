40060c68d18edb08874d374c4556d9501bef57cc
Yazid <yazeedkurdi@hotmail.com>
2023-05-06 22:18:20 +0300

    differentiate static component from service component

--- backend/settings.py
+++ backend/settings.py
@@ -230,8 +230,8 @@ if not DEBUG:
 
     FORCE_SCRIPT_NAME = '/app'
 
-    # Static files (CSS, JavaScript, Images)
-    # https://docs.djangoproject.com/en/4.2/howto/static-files/
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-    STATIC_URL = 'static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
