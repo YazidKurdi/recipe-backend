ecb12b4f877a30a5e5ff634edd06a1c5b63dade7
Yazid <yazeedkurdi@hotmail.com>
2023-05-06 21:15:34 +0300

    force script name

--- backend/settings.py
+++ backend/settings.py
@@ -231,5 +231,5 @@ FORCE_SCRIPT_NAME = '/app'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'app/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
