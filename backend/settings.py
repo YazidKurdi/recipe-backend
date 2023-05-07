ca96300731678e514d038fe7f0144a89b19f00af
Yazid <yazeedkurdi@hotmail.com>
2023-05-08 00:11:53 +0300

    app prefix

--- backend/settings.py
+++ backend/settings.py
@@ -228,8 +228,8 @@ LOGGING = {
 }
 
 
-# if not DEBUG:
-#     FORCE_SCRIPT_NAME = '/app'
+if not DEBUG:
+    FORCE_SCRIPT_NAME = '/app'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
