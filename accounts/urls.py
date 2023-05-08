b74733eb7b6dfd4e3e96145b9f8ce7df83149d88
Yazid <yazeedkurdi@hotmail.com>
2023-05-09 00:46:28 +0300

    remove comment accounts/backend urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-# from accounts import views
-#
-# urlpatterns = [
-#     path('get-userid/', views.GetUserIdView.as_view()),
-# ]
+from accounts import views
+
+urlpatterns = [
+    path('get-userid/', views.GetUserIdView.as_view()),
+]
