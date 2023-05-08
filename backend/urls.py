2f013cfeedfc30343e3e99b97fea422f47bf2d7d
Yazid <yazeedkurdi@hotmail.com>
2023-05-08 19:07:39 +0300

    comment accounts views

--- backend/urls.py
+++ backend/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
-from accounts.views import GoogleLogin #FacebookLogin, GithubLogin
+# from accounts.views import GoogleLogin #FacebookLogin, GithubLogin
 
 
 urlpatterns = [
