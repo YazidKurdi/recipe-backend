3aec4560ddffd08dbe0a4f9c829cec9e7094a41e
Yazid <yazeedkurdi@hotmail.com>
2023-05-10 14:25:02 +0300

    os join media root

--- backend/settings.py
+++ backend/settings.py
@@ -231,7 +231,7 @@ if not DEBUG:
     FORCE_SCRIPT_NAME = '/app'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'app/media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
