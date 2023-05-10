9ba5d9d8f1b8b53d511eeee66b33e61e4afb875d
Yazid <yazeedkurdi@hotmail.com>
2023-05-10 13:22:27 +0300

    remove app directory to media

--- backend/settings.py
+++ backend/settings.py
@@ -231,7 +231,7 @@ if not DEBUG:
     FORCE_SCRIPT_NAME = '/app'
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media/'
+MEDIA_ROOT = BASE_DIR / '/media/'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
