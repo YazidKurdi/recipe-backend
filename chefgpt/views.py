3f824b9b323c0e7f4d115be6674b4fb584bebc64
Yazid <yazeedkurdi@hotmail.com>
2023-05-20 16:09:08 +0300

    chefgpt api usage endpoint

--- chefgpt/views.py
+++ chefgpt/views.py
@@ -48,3 +48,39 @@ class ChefGPT(APIView):
         except Exception as e:
             logger.error(f"An error occurred: {str(e)}. gpt_response_quotes={generated_text}")
             return Response({'error': 'An error occurred.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+class APIUsage(APIView):
+    """
+    API endpoint that returns the current API usage.
+    """
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    throttle_classes = [ScopedRateThrottle]
+    throttle_scope = 'chef_gpt'
+
+    def get(self, request):
+        """
+        Returns the current API usage.
+        """
+        try:
+            throttle_instance = self.throttle_classes[0]()  # Create an instance of the throttle class
+
+            # throttle_key = throttle_instance.get_cache_key(request, self)
+            throttle_key = f"throttle_{self.throttle_scope}_{request.user.pk}"
+            throttle_history = throttle_instance.cache.get(throttle_key) or []
+
+            if throttle_history:
+                throttle_history.pop(0)  # Remove the first item from the list
+
+            throttle_instance.cache.set(throttle_key, throttle_history)  # Update the cache
+
+            # outstanding API calls
+            API_calls = len(throttle_history)
+
+            # API calls remaining
+            remaining_calls = utils.THROTTLE_RATE - API_calls
+
+            return Response(remaining_calls, status=status.HTTP_200_OK)
+        except:
+            return Response({'error': 'An error occurred.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
