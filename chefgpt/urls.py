60e3142558f064373be1952fa32db906e0a4121e
Yazid <yazeedkurdi@hotmail.com>
2023-05-20 16:07:30 +0300

    url gpt-usage

--- chefgpt/urls.py
+++ chefgpt/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from chefgpt import views
 
 urlpatterns = [
-    path('gpt-recipe/', views.ChefGPT.as_view())
+    path('gpt-recipe/', views.ChefGPT.as_view()),
+    path('gpt-usage/', views.APIUsage.as_view()),
 ]
